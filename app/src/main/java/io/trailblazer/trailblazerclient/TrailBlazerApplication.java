c534fe35ec9f2ca46b6ebfcef776ea74f6b192d8
Khizar Saleem <saleemkhizar@outlook.com>
2019-10-29 14:37:15 -0600

    Added Google Sign In

---
+++ app/src/main/java/io/trailblazer/trailblazerclient/TrailBlazerApplication.java
@@ -0,0 +1,14 @@
+package io.trailblazer.trailblazerclient;
+
+import android.app.Application;
+import io.trailblazer.trailblazerclient.service.GoogleSignInService;
+
+public class TrailBlazerApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    GoogleSignInService.setApplicationContext(this);
+
+  }
+}
