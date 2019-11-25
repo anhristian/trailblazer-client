78dce9a5d00c440151f0b070c90419e7f243d831
Khizar Saleem <saleemkhizar@outlook.com>
2019-11-25 14:14:10 -0700

    Added to NetworkService

--- app/src/main/java/io/trailblazer/trailblazerclient/service/NetworkService.java
+++ app/src/main/java/io/trailblazer/trailblazerclient/service/NetworkService.java
@@ -1,16 +1,23 @@
 package io.trailblazer.trailblazerclient.service;
 
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import io.trailblazer.trailblazerclient.model.Trail;
 import java.util.List;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
 
 public interface NetworkService {
 
   @GET("trails/public")
   Observable<List<Trail>> getAllTrails();
 
+  @GET("trails/")
+  Observable<List<Trail>> getAllAuthenticated(@Header("Authorization") String token);
+
+  @GET("trails/{id}")
+  Single<Trail> getTrailById(@Header("Authorization") String token, @Path("id") long id);
 
 
 }
