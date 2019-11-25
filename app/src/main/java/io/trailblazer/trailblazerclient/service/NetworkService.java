faa58bf53b8ba09a7fe44a324193c3b495c5b48b
Khizar Saleem <saleemkhizar@outlook.com>
2019-11-25 15:10:04 -0700

    Added to NetworkService

--- app/src/main/java/io/trailblazer/trailblazerclient/service/NetworkService.java
+++ app/src/main/java/io/trailblazer/trailblazerclient/service/NetworkService.java
@@ -1,17 +1,30 @@
 package io.trailblazer.trailblazerclient.service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.trailblazer.trailblazerclient.BuildConfig;
 import io.trailblazer.trailblazerclient.model.Trail;
 import java.util.List;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface NetworkService {
 
   @GET("trails/public")
   Observable<List<Trail>> getAllTrails();
+
+  @GET("trails/mytrails")
+  Observable<List<Trail>> getMyTrails(@Header("Authorization") String token);
+
+  @GET("trails/search")
+  Observable<List<Trail>> getTrailsByName(@Header("Authorization") String token, @Query("name") String name);
 
   @GET("trails/")
   Observable<List<Trail>> getAllAuthenticated(@Header("Authorization") String token);
@@ -19,5 +32,29 @@ public interface NetworkService {
   @GET("trails/{id}")
   Single<Trail> getTrailById(@Header("Authorization") String token, @Path("id") long id);
 
+
+
+
+  static NetworkService getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  class InstanceHolder {
+
+    private static final NetworkService INSTANCE;
+
+    static {
+      Gson gson = new GsonBuilder()
+          .excludeFieldsWithoutExposeAnnotation()
+          .create();
+      Retrofit retrofit = new Retrofit.Builder()
+          .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+          .addConverterFactory(GsonConverterFactory.create(gson))
+          .baseUrl(BuildConfig.BASE_URL)
+          .build();
+      INSTANCE = retrofit.create(NetworkService.class);
+    }
+
+  }
 
 }
