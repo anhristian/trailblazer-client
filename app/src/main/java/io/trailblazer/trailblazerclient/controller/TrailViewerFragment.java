80321b685234872cdb75cb2890628021007ac492
justinbdom <justinbdominguez1990@gmail.com>
2019-11-27 10:47:48 -0700

    Added icon image to res.

--- app/src/main/java/io/trailblazer/trailblazerclient/controller/TrailViewerFragment.java
+++ app/src/main/java/io/trailblazer/trailblazerclient/controller/TrailViewerFragment.java
@@ -1,5 +1,6 @@
 package io.trailblazer.trailblazerclient.controller;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +13,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import io.trailblazer.trailblazerclient.R;
 import io.trailblazer.trailblazerclient.model.Trail;
+import io.trailblazer.trailblazerclient.view.TrailAdapter;
 import io.trailblazer.trailblazerclient.viewmodel.TrailViewViewModel;
 
 public class TrailViewerFragment extends Fragment {
