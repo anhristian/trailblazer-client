d6817736e5491dd058b6d56ff479625f4c399b1b
Khizar Saleem <saleemkhizar@outlook.com>
2019-12-02 15:11:10 -0700

    Added to ProfileFragment

--- app/src/main/java/io/trailblazer/trailblazerclient/controller/ProfileFragment.java
+++ app/src/main/java/io/trailblazer/trailblazerclient/controller/ProfileFragment.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 import io.trailblazer.trailblazerclient.R;
+import io.trailblazer.trailblazerclient.model.User;
+import io.trailblazer.trailblazerclient.viewmodel.UserViewModel;
 
 
 public class ProfileFragment extends Fragment {
@@ -15,13 +20,44 @@ public class ProfileFragment extends Fragment {
 
   private View view;
   private Context context;
+  private UserViewModel userViewModel;
+  private User user;
+  private ImageView editProfile;
+  private EditText editName;
+  private EditText editAge;
+  private EditText editWeight;
+  private EditText editHeight;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
+    userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
     view = inflater.inflate(R.layout.profile_fragment, container, false);
     context = container.getContext();
+    editProfile = view.findViewById(R.id.edit);
+    editName = view.findViewById(R.id.edit_name);
+    editAge = view.findViewById(R.id.edit_age);
+    editWeight = view.findViewById(R.id.edit_weight);
+    editHeight = view.findViewById(R.id.edit_height);
+    editName.setEnabled(false);
+    editAge.setEnabled(false);
+    editWeight.setEnabled(false);
+    editHeight.setEnabled(false);
+//    user = (User) getArguments().getSerializable("user");
+//    if (user == null) {
+//      user = new User();
+//    }
+    if (savedInstanceState == null) {
+      populateFields();
+    }
+
     return view;
+  }
+
+  private void populateFields() {
+    if (user.getUsername() != null) {
+      editName.setText(user.getUsername());
+    }
   }
 
 }
